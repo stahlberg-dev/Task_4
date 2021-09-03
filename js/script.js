806419584b6352e862d112fee517dcf0417c7738
Dmitrii Stahlberg <stahlberg@mail.ru>
2021-09-03 14:17:51 +0300

    add pagination

--- js/script.js
+++ js/script.js
@@ -13,36 +13,46 @@ $(document).ready(function(){
         });    
     });
 
+    showPage($('.order'), 1);
+
     $('.menu__toggle').on('click', ToggleMenu);
     $('.overlay').on('click', ToggleMenu);
     $('.menu__item').each(function(i) {
         $(this).on('click', function() {
             $('.menu__active-item').text($('.menu__item').eq(i).text());
             ToggleMenu();  
+            let $delivered = $('.order[data-status="delivered"]');
+            let $cancelled = $('.order[data-status="cancelled"]');
+            let $active = $('.order:not([data-status="delivered"]):not([data-status="cancelled"])');
+
             switch (i) {
                 case 0:
                     $('.order').show(100);
+                    showPage($('.order'), 1);
                     break;
                 case 1:
-                    $('.order[data-status="delivered"] .order-body').slideUp();
-                    $('.order[data-status="cancelled"] .order-body').slideUp();
-                    $('.order[data-status="delivered"]').find('.arrow.up').removeClass('up');
-                    $('.order[data-status="cancelled"]').find('.arrow.up').removeClass('up');
-                    $('.order[data-status="delivered"]').hide(100);
-                    $('.order[data-status="cancelled"]').hide(100);
-                    $('.order:not([data-status="delivered"]):not([data-status="cancelled"])').show(100);
+                    $('.order-body', $delivered).slideUp();
+                    $('.order-body', $cancelled).slideUp();
+                    $delivered.find('.arrow.up').removeClass('up');
+                    $cancelled.find('.arrow.up').removeClass('up');
+                    $delivered.hide(100);
+                    $cancelled.hide(100);
+                    $active.show(100);
+                    showPage($active, 1);
                     break;
                 case 2:
-                    $('.order:not([data-status="delivered"]) .order-body').slideUp();
-                    $('.order:not([data-status="delivered"])').find('.arrow.up').removeClass('up');
-                    $('.order:not([data-status="delivered"])').hide(100);
-                    $('.order[data-status="delivered"]').show(100);
+                    $('.order-body', $('.order').not($delivered)).slideUp();
+                    $('.order').not($delivered).find('.arrow.up').removeClass('up');
+                    $('.order').not($delivered).hide(100);
+                    $delivered.show(100);
+                    showPage($delivered, 1);
                     break;
                 case 3:
-                    $('.order:not([data-status="cancelled"]) .order-body').slideUp();
-                    $('.order:not([data-status="cancelled"])').find('.arrow.up').removeClass('up');
-                    $('.order:not([data-status="cancelled"])').hide(100);
-                    $('.order[data-status="cancelled"]').show(100);
+                    $('.order-body', $('.order').not($cancelled)).slideUp();
+                    $('.order').not($cancelled).find('.arrow.up').removeClass('up');
+                    $('.order').not($cancelled).hide(100);
+                    $cancelled.show(100);
+                    showPage($cancelled, 1);
                     break;
             } 
         });
@@ -65,4 +75,95 @@ function showCard() {
 
     $(this).next().slideToggle(200);
     $(this).find('.arrow').toggleClass('up');
+}
+
+function showPage(orders, page) {
+    let ordersPerPage = 10;
+    let numPage = numPages(orders, ordersPerPage);
+    orders.slice(0, (page-1) * ordersPerPage).each(function() {
+        $(this).hide();
+        $('.order-body', $(this)).slideUp();
+        $(this).find('.arrow.up').removeClass('up');
+    });
+    orders.slice(((page-1) * ordersPerPage) + ordersPerPage).each(function() {
+        $(this).hide();
+        $('.order-body', $(this)).slideUp();
+        $(this).find('.arrow.up').removeClass('up');
+    });
+    orders.slice((page-1) * ordersPerPage,
+    ((page-1) * ordersPerPage) + ordersPerPage).each(function() {
+        $(this).show();
+    });
+    renderControls(orders, '.contols-container', page, numPage);
+}
+
+function numPages(orders, ordersPerPage) {
+    return Math.ceil(orders.length / ordersPerPage);
+}
+
+function renderControls(orders, container, currentPage, numPages) {
+    if (numPages > 1) {
+        $(container).html("<ul class = 'controls'></ul>");   
+        if (numPages < 6) {            
+            for (let i = 1; i <= numPages; i++) {
+                if (i == currentPage) {
+                    $('ul.controls', container).append($('<li>', {
+                        class : 'controls__page-number current',
+                        text : ''+ i +'',         
+                    }));
+                } else {
+                    $('ul.controls', container).append($('<li>', {
+                        class : 'controls__page-number',
+                        text : ''+ i +'',
+                        click : function() {
+                                            showPage(orders, i);
+                                            return true;
+                                        }                           
+                    }));
+                }
+            } 
+        } else {
+            for (let i = 1; i <= numPages; i++) {
+                if (i == currentPage) {
+                    $('ul.controls', container).append($('<li>', {
+                        class : 'controls__page-number current',
+                        text : ''+ i +'',         
+                    }));
+                } else {
+                    if ((i >= 1 && i <= 3) || i == numPages || i - currentPage == 1 || i - currentPage == -1) {
+                        $('ul.controls', container).append($('<li>', {
+                            class : 'controls__page-number',
+                            text : ''+ i +'',
+                            click : function() {
+                                                showPage(orders, i);
+                                                return true;
+                                            }                           
+                        }));
+                    } else {
+                        if (i - currentPage == -2 || i - numPages == -1){
+                            $('ul.controls', container).append($('<li>', {
+                                class : 'controls__space',
+                                text : '...'  
+                            }));
+                        }
+                    }
+                }
+            } 
+        }
+        $('ul.controls', container).append($('<li>', {
+            class : 'controls__next',
+            text : 'дальше',
+            click : function() {
+                                if (currentPage == numPages) {
+                                    showPage(orders, 1);
+                                    return true;
+                                } else {
+                                    showPage(orders, currentPage + 1);
+                                    return true;
+                                }
+                            }                           
+        }));
+    } else {
+        $(container).html('');
+    }
 }
